e8e06aac00cd6ec897ddce68e80a7b070a14646b
Carlos Resende <resende.carlinhos123@gmail.com>
2026-04-14 15:59:51 -0300

    corrige caminhos das imagens no css

--- .vscode/script.js
+++ .vscode/script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode adicionar o avatar 2
-    img.setAttribute("src", "./assets/avatar2.png.jpeg")
+    img.setAttribute("src", "assets/avatar2.png.jpeg")
   } else {
     // se tiver sem light mode manter a original
-    img.setAttribute("src", "./assets/avatar.png.jpeg")
+    img.setAttribute("src", "assets/avatar.png.jpeg")
   }
 }
