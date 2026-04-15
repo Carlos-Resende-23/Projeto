582c4eb706d4b4c22ab0a35817041dcb945302c0
Carlos Resende <resende.carlinhos123@gmail.com>
2026-04-15 14:32:25 -0300

    fix tema inicial

--- .vscode/script.js
+++ .vscode/script.js
@@ -2,15 +2,9 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // pegar a tag img
-  const img = document.querySelector("#profile img")
-
-  // substituir a imagem
   if (html.classList.contains("light")) {
-    // se tiver light mode adicionar o avatar 2
-    img.setAttribute("src", "assets/avatar2.png.jpeg")
+    localStorage.setItem("theme", "light")
   } else {
-    // se tiver sem light mode manter a original
-    img.setAttribute("src", "assets/avatar.png.jpeg")
+    localStorage.setItem("theme", "dark")
   }
 }
