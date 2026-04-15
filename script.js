b5fa678811d56fbe04fb5044d8239c37e721d023
Carlos Resende <resende.carlinhos123@gmail.com>
2026-04-15 15:56:12 -0300

    fix definitivo DOMContentLoaded

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-const html = document.documentElement
-const img = document.querySelector("#profile img")
+document.addEventListener("DOMContentLoaded", function () {
+  const html = document.documentElement
+  const img = document.querySelector("#profile img")
 
-function initTheme() {
-  const temaSalvo = localStorage.getItem("theme")
+  function applyTheme() {
+    const temaSalvo = localStorage.getItem("theme")
 
-  if (temaSalvo === "light") {
-    html.classList.add("light")
-    img.src = "./assets/avatar2.png.jpeg"
-  } else {
-    img.src = "./assets/avatar.png.jpeg"
+    if (temaSalvo === "light") {
+      html.classList.add("light")
+      img.src = "./assets/avatar2.png.jpeg"
+    } else {
+      html.classList.remove("light")
+      img.src = "./assets/avatar.png.jpeg"
+    }
   }
-}
 
-function toggleMode() {
-  html.classList.toggle("light")
+  applyTheme()
 
-  if (html.classList.contains("light")) {
-    img.src = "./assets/avatar2.png.jpeg"
-    localStorage.setItem("theme", "light")
-  } else {
-    img.src = "./assets/avatar.png.jpeg"
-    localStorage.setItem("theme", "dark")
+  window.toggleMode = function () {
+    html.classList.toggle("light")
+
+    if (html.classList.contains("light")) {
+      img.src = "./assets/avatar2.png.jpeg"
+      localStorage.setItem("theme", "light")
+    } else {
+      img.src = "./assets/avatar.png.jpeg"
+      localStorage.setItem("theme", "dark")
+    }
   }
-}
+})
