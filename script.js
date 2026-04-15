8cdd75b2e41620ce6bf1e64f49dda9ce258d3267
Carlos Resende <resende.carlinhos123@gmail.com>
2026-04-15 15:31:08 -0300

    corrige script duplicado

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-function toggleMode() {
-  const html = document.documentElement
-  const img = document.getElementById("avatar")
+const html = document.documentElement
+const img = document.querySelector("#profile img")
 
+// aplicar tema salvo ao carregar
+const temaSalvo = localStorage.getItem("theme")
+if (temaSalvo === "light") {
+  html.classList.add("light")
+  img.setAttribute("src", "./assets/avatar2.png.jpeg")
+} else {
+  img.setAttribute("src", "./assets/avatar.png.jpeg")
+}
+
+function toggleMode() {
   html.classList.toggle("light")
 
   if (html.classList.contains("light")) {
-    img.src = "./assets/avatar2.png.jpeg"
+    img.setAttribute("src", "./assets/avatar2.png.jpeg")
     localStorage.setItem("theme", "light")
   } else {
-    img.src = "./assets/avatar.png.jpeg"
+    img.setAttribute("src", "./assets/avatar.png.jpeg")
     localStorage.setItem("theme", "dark")
-  }
-}
-
-window.onload = function () {
-  const temaSalvo = localStorage.getItem("theme")
-  const img = document.getElementById("avatar")
-
-  if (temaSalvo === "light") {
-    document.documentElement.classList.add("light")
-    img.src = "./assets/avatar2.png.jpeg"
-  } else {
-    img.src = "./assets/avatar.png.jpeg"
   }
 }
