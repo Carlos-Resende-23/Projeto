d84da96aac408568b51e47b495bf49afca4ae9c7
Carlos Resende <resende.carlinhos123@gmail.com>
2026-04-15 15:04:10 -0300

    envia ultima correcao do avatar

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const img = document.getElementById("avatar")
 
 if (temaSalvo === "light") {
   document.documentElement.classList.add("light")
-  if (img) img.src = "./assets/avatar2.png"
+  if (img) img.src = "./assets/avatar2.png.jpeg"
 }
 
 function toggleMode() {
@@ -13,10 +13,10 @@ function toggleMode() {
   const img = document.getElementById("avatar")
 
   if (html.classList.contains("light")) {
-    img.src = "./assets/avatar2.png"
+    img.src = "./assets/avatar2.png.jpeg"
     localStorage.setItem("theme", "light")
   } else {
-    img.src = "./assets/avatar.png"
+    img.src = "./assets/avatar.png.jpeg"
     localStorage.setItem("theme", "dark")
   }
 }
