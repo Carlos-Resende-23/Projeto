ef793f37bf2c7dda0dd36bf8b07583c88dc2d7a9
Carlos Resende <resende.carlinhos123@gmail.com>
2026-04-15 15:11:24 -0300

    fix definitivo toggle e avatar

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const temaSalvo = localStorage.getItem("theme")
-const img = document.getElementById("avatar")
-
-if (temaSalvo === "light") {
-  document.documentElement.classList.add("light")
-  if (img) img.src = "./assets/avatar2.png.jpeg"
-}
-
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("light")
-
   const img = document.getElementById("avatar")
+
+  html.classList.toggle("light")
 
   if (html.classList.contains("light")) {
     img.src = "./assets/avatar2.png.jpeg"
@@ -18,5 +10,18 @@ function toggleMode() {
   } else {
     img.src = "./assets/avatar.png.jpeg"
     localStorage.setItem("theme", "dark")
+  }
+}
+
+// Corrige ao carregar a página
+window.onload = function () {
+  const temaSalvo = localStorage.getItem("theme")
+  const img = document.getElementById("avatar")
+
+  if (temaSalvo === "light") {
+    document.documentElement.classList.add("light")
+    img.src = "./assets/avatar2.png.jpeg"
+  } else {
+    img.src = "./assets/avatar.png.jpeg"
   }
 }
