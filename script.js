8a22b4b94960661f2bdbc60d69023bcf1db235b7
Carlos Resende <resende.carlinhos123@gmail.com>
2026-04-15 14:59:19 -0300

    fix troca do avatar

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const temaSalvo = localStorage.getItem("theme")
+const img = document.getElementById("avatar")
+
+if (temaSalvo === "light") {
+  document.documentElement.classList.add("light")
+  if (img) img.src = "./assets/avatar2.png"
+}
+
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
+  const img = document.getElementById("avatar")
+
   if (html.classList.contains("light")) {
+    img.src = "./assets/avatar2.png"
     localStorage.setItem("theme", "light")
   } else {
+    img.src = "./assets/avatar.png"
     localStorage.setItem("theme", "dark")
   }
 }
